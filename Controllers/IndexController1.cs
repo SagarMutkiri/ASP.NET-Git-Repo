9a37d22005d0b7c4b25cd35ac8cf3f07d591c4ba
SagarMutkiri <93123228+SagarMutkiri@users.noreply.github.com>
2022-02-23 15:11:43 +0530

    version-1.2

---
+++ Controllers/IndexController1.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Session_ASP_Git.Controllers
+{
+    public class IndexController1 : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
