f0dba1289c881fb64817a2d3c55adaa4e12a9f4c
SagarMutkiri <93123228+SagarMutkiri@users.noreply.github.com>
2022-02-23 15:04:14 +0530

    version-1.1

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Session_ASP_Git.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
